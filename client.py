d0888b8bb01caf7628b01b2bc42d826f13bd3b60
ranavaibhav <vaibhavranadon@gmail.com>
2016-07-09 19:32:04 +0530

    third edit

--- client.py
+++ client.py
@@ -3,15 +3,21 @@ import socket
 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host= socket.gethostname()
-port= 8888
+port= 8000
 
 c.connect((host,port))
+
 tm= c.recv(1024)
-while True:
-	a= raw_input('message: ')
-	serversocket.send(a)
-	if a == 'end'
-	    break
-	    print 'bbye'
-	    c.close()
+
 print("time is %s" % tm.decode('ascii'))
+
+while True:
+    a= raw_input('message: ')
+    c.send(a)
+    b= c.recv(16)
+    print "recieved message:",b
+    if a == 'end':
+        print ('bbye')
+        c.close()
+        print("time is %s" % tm.decode('ascii'))
+        break
