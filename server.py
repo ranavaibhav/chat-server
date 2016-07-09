563f180f217f0a62c6158d18a4407722cd8202e2
ranavaibhav <vaibhavranadon@gmail.com>
2016-07-09 19:31:45 +0530

    third edit

--- server.py
+++ server.py
@@ -4,20 +4,27 @@ import time
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
-port=8888
+port=8000
 
 serversocket.bind((host,port))
 serversocket.listen(5)
 
+clientsocket, addr = serversocket.accept()
+print ("got a connection from %s" % str(addr))
+currenttime = time.ctime(time.time()) + "\r\n"
+clientsocket.send(currenttime.encode('ascii'))
+
 while True:
-	clientsocket, addr = serversocket.accept()
-	print ("got a connection from %s" % str(addr))
-	currenttime = time.ctime(time.time()) + "\r\n"
-	clientsocket.send(currenttime.encode('ascii'))
-while True:
-	a= raw_input('message: ')
-	clientsocket.send(a)
-	if a == 'end'
-	    break
-	    print 'bbye', currenttime = time.ctime(time.time()) + "\r\n"
-	clientsocket.close()
+    b= clientsocket.recv(16)
+
+    print "recieved message:",b
+    
+    a= raw_input('message: ')
+
+    clientsocket.send(a)
+    
+    if a == 'end':
+        print 'bbye'
+        currenttime = time.ctime(time.time()) + "\r\n"
+        clientsocket.close()
+        break
